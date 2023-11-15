18b8aac1ad4b411b429e991df62893177772b7c0
darkdeath <darkdeathoriginal@gmail.com>
2023-11-15 23:29:42 +0530

    start command changes

--- bot/utils/defaultCommands.js
+++ bot/utils/defaultCommands.js
@@ -1,20 +1,23 @@
 const startCommand = {
-    pattern:"start",
-    description:"Start command",
-    callback:async (message,match) => {
-        console.log(this);
-        await message.send("Bot is started!");
+  pattern: "start",
+  description: "Start command",
+  callback: async (message, match, obj) => {
+    let msg = "Bot is started!\n\nBot commands:\n";
+    for (let i of obj.modules) {
+      msg += `/${i.pattern} - ${i.description}\n`;
     }
-}
+    await message.send(msg);
+  },
+};
 
 const pingCommand = {
-    pattern: "ping",
-    description: "Ping command",
-    callback: async (m, match) => {
-      let start = new Date().getTime();
-      await m.send(`❮ ᴛᴇsᴛɪɴɢ ᴘɪɴɢ ❯`);
-      let end = new Date().getTime();
-      await m.send(`ʟᴀᴛᴇɴᴄʏ: ${end - start} ᴍs`);
-    },
-  }
-module.exports = {startCommand,pingCommand}
+  pattern: "ping",
+  description: "Ping command",
+  callback: async (m, match) => {
+    let start = new Date().getTime();
+    await m.send(`❮ ᴛᴇsᴛɪɴɢ ᴘɪɴɢ ❯`);
+    let end = new Date().getTime();
+    await m.send(`ʟᴀᴛᴇɴᴄʏ: ${end - start} ᴍs`);
+  },
+};
+module.exports = { startCommand, pingCommand };
